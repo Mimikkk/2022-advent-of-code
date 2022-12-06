61c0fb05a280f5920e72b9dee0f3acf36588f585
Hououin Kyouma <inwazzin@gmail.com>
2022-12-06 19:07:35 +0100

    fix depth

--- src/days/day-6/index.ts
+++ src/days/day-6/index.ts
@@ -1,53 +1,32 @@
-import _, {
-  filter,
-  isEmpty,
-  split,
-  identity,
-  map,
-  pipe,
-  negate,
-  toNumber,
-  last,
-  range,
-  isEqual,
-  reverse,
-  join,
-  concat,
-  takeLast,
-  dropLast,
-  tap,
-  each,
-  first,
-  find,
-  findIndex,
-  uniq,
-  add
-} from "lodash/fp";
+import { split, pipe, join, findIndex, uniq, add } from "lodash/fp";
 import { day, Preprocess } from "../../utils";
 
 const windowGenerator = function* <T>(items: T[], size: number) {
-  for (let i = 0; i + size <= items.length; ++i)
-    yield items.slice(i, i + size);
+  for (let i = 0; i + size <= items.length; ++i) yield items.slice(i, i + size);
 };
 
-const windows = (size: number) => <T>(items: T[]) => Array.from(windowGenerator(items, size));
+const windows =
+  (size: number) =>
+  <T>(items: T[]) =>
+    Array.from(windowGenerator(items, size));
 
-const findIndexOfFirstNUniqueCharacters = (n: number) => pipe(
-  windows(n),
-  findIndex((chars) => uniq(chars).length === n),
-  add(n)
-);
+const findIndexOfFirstNUniqueCharacters = (n: number) =>
+  pipe(
+    windows(n),
+    findIndex((chars) => uniq(chars).length === n),
+    add(n)
+  );
 
-const preprocess = pipe(join(""), split("")) satisfies Preprocess<string[]>
+const preprocess = pipe(join(""), split("")) satisfies Preprocess<string[]>;
 export default day({
   first: {
     preprocess,
     solution: findIndexOfFirstNUniqueCharacters(4),
-    expected: 7
+    expected: 7,
   },
   second: {
     preprocess,
     solution: findIndexOfFirstNUniqueCharacters(14),
-    expected: 19
-  }
+    expected: 19,
+  },
 });
